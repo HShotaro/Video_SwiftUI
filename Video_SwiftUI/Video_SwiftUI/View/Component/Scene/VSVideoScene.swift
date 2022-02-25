6060038474fb49044806f5e4dde84186a2a4472b
HShotaro <shotaro.10965@gmail.com>
2022-02-25 14:16:10 +0900

    SKActionを編集した

--- Video_SwiftUI/Video_SwiftUI/View/Component/Scene/VSVideoScene.swift
+++ Video_SwiftUI/Video_SwiftUI/View/Component/Scene/VSVideoScene.swift
@@ -12,28 +12,29 @@ class VSVideoScene: SKScene {
     
     override func didMove(to view: SKView) {
 //        物体が落下した時に下に積もるようにしたい時に使う
-//        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
-        let node = SKSpriteNode(imageNamed: "petal")
-        node.position = location
-        node.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        let petalNode = SKSpriteNode(imageNamed: "petal")
+        petalNode.position = location
+        petalNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
         let nodeName = "petal_" + UUID().uuidString
-        node.name = nodeName
-        addChild(node)
+        petalNode.name = nodeName
+        addChild(petalNode)
         rotate(nodeName: nodeName)
     }
     
     private func rotate(nodeName: String) {
         guard let petalNode = self.childNode(withName: nodeName) as? SKSpriteNode else { return }
-        let action1 = SKAction.wait(forDuration: 0.2)
-        let action2 = SKAction.rotate(byAngle: CGFloat(Double.pi * 5), duration: 1.0)
+        let action1 = SKAction.rotate(byAngle: CGFloat(Double.pi * 4), duration: 1.0)
+        let action2 = SKAction.wait(forDuration: 2)
         //一定速度で回転する。
         action2.timingMode = SKActionTimingMode.linear
-        let actionAll = SKAction.sequence([action1,action2])
+        let action3 = SKAction.removeFromParent()
+        let actionAll = SKAction.sequence([action1, action2, action3])
         petalNode.run(actionAll)
     }
 }
--- Video_SwiftUI/Video_SwiftUI/View/CustomEffect/UIKit/VSCustomEffectViewRepresentable.swift
+++ Video_SwiftUI/Video_SwiftUI/View/CustomEffect/UIKit/VSCustomEffectViewRepresentable.swift
@@ -36,6 +36,6 @@ struct VSCustomEffectViewRepresentable: UIViewControllerRepresentable {
 
 struct VSCustomEffectViewRepresentable_Previews: PreviewProvider {
     static var previews: some View {
-        VSCustomEffectViewRepresentable(playerItem: AVPlayerItem(url: URL(string: "file:///Users/s.hirano/Library/Developer/CoreSimulator/Devices/478B0B86-EE42-47C7-BA65-84C285C768ED/data/Containers/Shared/AppGroup/C5238E37-D733-4510-BF0E-EB01D63E723A/File%20Provider%20Storage/photospicker/version=1&uuid=EA5175EE-C703-49B5-80BE-05072235A56F&mode=current.mp4")!))
+        VSCustomEffectViewRepresentable(playerItem: AVPlayerItem(url: URL(string: "")!))
     }
 }
